ffc8ed4f8fd0c40f9162092b0087511b21ef0a4e
Alex Shepherd <alex@thefrontend.uk>
2020-03-06 12:53:25 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'react-hooks'],
+  ignorePatterns: ['__tests__/'],
+  rules: {
+    'react/prop-types': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# eslint config for react
+
+In your ``.eslintrc.js``
+
+```js
+module.exports = {
+  extends: ['@thefrontend/eslint-config-react']
+}
+```
